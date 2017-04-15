54d600f6fdceefd9a4be186f94b09a5a4c071a77
Xxedwin <edwin_limache@hotmail.com>
2017-04-15 18:25:12 -0500

    maquetando registro de usuario..

--- include/navbar.php
+++ include/navbar.php
@@ -1,4 +1,22 @@
 <?php 
-
+	$name = isset($_SESSION["name"])? $_SESSION["name"]:"";
+	$email = isset($_SESSION["email"])? $_SESSION["email"]:"";
 
  ?>
+
+ <header class="header-encabezado">
+	<nav class="header-quiz">
+		<?php 
+			if (!$email) {
+		?>
+			<div class="">
+				<img class="quiz" src="img/quiz.png">
+			</div>
+			<button>LOGIN</button>
+		<?php
+			}else{
+
+			}
+		?>
+	</nav>
+</header>
--- login.php
+++ login.php
@@ -2,24 +2,41 @@
 	
 	include 'config/db.php';
 
+	require 'vendor/autoload.php';
+
+	session_start();
+
 	$email = isset($_REQUEST['email']) ? $_REQUEST['email']:'';
 	$password = isset($_REQUEST['password']) ? $_REQUEST['password']:'';
 	$type = isset($_REQUEST['type'])? $_REQUEST['type']:'';
 
 	if($email&&$password){
 
-		$sql = "SELECT * FROM users WHERE email= ".$email. " AND password=".$password;
+		try {
+			$sql = "SELECT * FROM users WHERE email= '".$email."' LIMIT 1";
 
-		$result = $con->prepare($sql); 
-		$result->execute(); 
-		$numRows = $result->fetchColumn(); 
+			$result = $con->prepare($sql); 
+			$result->execute(); 
 
-		if($numRows>0){
-
+			$row = $result->fetch();
+		} catch (Exception $e) {
+			echo $e->getMessage();
 		}
+
+		if(!$row){
+			header('Location: index.php');
+		}
+
+		if (PHPassLib\Hash\BCrypt::verify($password, $row["password"])) {
+
+			$_SESSION["name"] = $row["name"]." ".$row["lastname"];
+			$_SESSION["id"] = $row["id"];
+			$_SESSION["email"] = $row["email"];
+			header('Location: question.php');			
+		}
+
 	}else{
 		header('Location: index.php');
 	}
-
 
  ?>
