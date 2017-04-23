3d02aee52b7bf61c439cc28bef741974ccd4f1a7
Franz <fcruzlau@gmail.com>
2017-04-22 19:19:51 -0500

    corre modal

--- main.php
+++ main.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 
 	session_start();
 
@@ -15,9 +15,9 @@
 	<title>quizApp</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
-<body>
-
-
+<body>	
+	
+	
 	<?php include 'include/navbar.php'; ?>
 
 	<div class="conteiner">
@@ -27,141 +27,71 @@
 			<p class="parrafo-quiz">Esta aplicacion mejora tu capacidad de responder preguntas rapidamente y con esta aplicacion web aprenderas cosas nuevas sobre cultura general</p>
 			<p class="parrafo-quiz">Esta aplicacion mejora tu capacidad de responder preguntas rapidamente y con esta aplicacion web aprenderas cosas nuevas sobre cultura general</p>
 		</div>
-		<div class="login">
-			<form method="POST" action="login.php">
-				<div class="checkName">
-					<div class="form-submit">
-						<button class="btn" id="fb-login">
-							<img width="20" height="20" src="img/facebook.ico">
-							<span>INICIAR SESION CON FACEBOOK</span>
-						</button>
-					</div>
-					<div class="form-style">
-						<input class="input" type="email" name="email" placeholder="Email">
-					</div>
-					<div class="form-style">
-						<input class="input" type="password" name="password" placeholder="Contrasena">
-					</div>
-					<div class="form-submit">
-						<button class="btn btn-medium">INICIAR SESION</button>
-					</div>
-					<difv id="finestra-modal-obrir1" class="form-submit ">
-						<button class="btn btn-medium">REGISTRARTE</button>
-					</div>
-
-					<span><a id="finestra-modal-obrir1" class="finestra-modal-obrir1 boto1">ABRIR</a></span>
-				</div>
+		<div class="content">
+		    <div class="login">	
+		    <button class="login-facebook">Login FACEBOOK</button>		
+			<form action="login.php" method="POST" id="frm-intro">
+			    <input type="email" name="email" placeholder="Email">
+			 
+			    <input type="password" placeholder="Contraseña">
 			</form>
+            <button type="submit" class="btn-sesion">INICIAR SESIÓN</button>
+            <button id="register1" class="btn-sesion">REGISTRATE</button>
+			
+		    </div>
+		</div>	
+		
+		
+		<!--AQUI INICIA EL MODAL PARA REGISTRO000-->
+		
+        <div id="finestra-modal1" class="finestra-modal1 ">
+          
+            <div class="finestra-modal__contingut1">        
+                <div  class="finestra-modal-tancar1 boto1">×</div> 
+                <h1 class="modal-registrate">Registrate</h1>
+                <form id="register-form">
+                        <p>Ingrese el Nombre</p>
+				        <input type="text" placeholder="Nombre">
+				        <p>Ingrese el Apellido</p>
+				        <input type="text" placeholder="Apellido">
+                        <p>Ingrese el Correo Electronico</p>
+				        <input type="text" placeholder="Correo ELectronico">
+				        <p>Ingrese la Contrasena</p>
+				        <input type="password" placeholder="Contrasena">
+						<button class="register-enviar" >ENVIAR</button>
+						<button class="" id="btn-cerrar">CERRAR</button>
+				</form>	   
+                
+            </div>   
+        </div>
+        <!--Aqui TERMINA EL MODALLLLLLL ------->	
 		</div>
-		<!-- <div id="">
-			<div id="finestra-modal1" class="finestra-modal1 js-mostrar1">
-			  <div class="finestra-modal__contingut1">
-			      <div id="finestra-modal-tancar1" class="finestra-modal-tancar1 boto1">×</div>
-
-			      	<div>Registrate</div>
-					<form id="register-form">
-						<div class="form-register">
-							<p>Ingrese el Nombre</p>
-							<input type="text" name="name" placeholder="Nombre">
-						</div>
-						<div class="form-register">
-							<p>Ingrese el Apellido</p>
-							<input type="text" name="lastname" placeholder="Apellido">
-						</div>
-						<div class="form-register">
-							<p>Ingrese el Correo Electronico</p>
-							<input type="text" name="email" placeholder="Correo ELectronico">
-						</div>
-						<div class="form-register">
-							<p>Ingrese la Contrasena</p>
-							<input type="password" name="password" placeholder="Contrasena">
-						</div>
-						<div>
-							<button type="submit" class="register-enviar" >ENVIAR</button>
-						</div>
-					</form>
-			       </div>
-			  </div>
-			</div>
-		</div> -->
-
-	<script src="js/index.js"></script>
+	
+	<script src="js/modalregister.js"></script>
 	<script type="text/javascript">
-
-		// document.getElementById("register-form").addEventListener("submit",function(e) {
-		// 	e.preventDefault();
-		// 	e.stopPropagation();
-		// 	debugger;
-		// 	var $this = this;
-
-		// 	var data = new FormData(this);
-
-		// 	var xHttp = new XMLHttpRequest();
-
-		// 	xHttp.open("POST","register.php");
-
-		// 	xHttp.onreadystatechange  = function() {
-		// 		if (this.readyState == 4 && this.status == 200) {
-		// 			location.reload();
-		// 	 	}
-		// 	}
-
-		// 	xHttp.send(data);
-
-		// });
-	</script>
-	<script type="text/javascript">
-
-	  window.fbAsyncInit = function() {
-	    FB.init({
-	      appId      : '202934583530333',
-	      xfbml      : true,
-	      version    : 'v2.8'
-	    });
-	    FB.AppEvents.logPageView();
-	  };
-
-	  (function(d, s, id){
-	     var js, fjs = d.getElementsByTagName(s)[0];
-	     if (d.getElementById(id)) {return;}
-	     js = d.createElement(s); js.id = id;
-	     js.src = "//connect.facebook.net/en_US/sdk.js";
-	     fjs.parentNode.insertBefore(js, fjs);
-	   }(document, 'script', 'facebook-jssdk'));
-	</script>
-	<script type="text/javascript">
-
-		document.getElementById("fb-login").onclick = function(e) {
-			e.preventDefault();
-			e.stopPropagation();
+		
 
 
+		document.getElementById("register-form").addEventListener("submit",function() {
+			var $this = this;
+			var data = {
+				name : this.name.value,
+				lastname: this.lastname.value,
+				email: this.email.value,
+				password: this.password.value
+			};
 
-			FB.login(function(response){
-				FB.api('/me?fields=id,email,first_name,last_name', function(response) {
-			      	debugger;
-					var data = new FormData(this);
-					data.append("facebookId",response.id);
-					data.append("name",response.first_name);
-					data.append("lastname",response.last_name);
-					data.append("email",response.email);
-					var xHttp = new XMLHttpRequest();
+			var xHttp = new XMLHttpRequest();
+			xHttp.onreadystatechange  = function() {
+				if (this.readyState == 4 && this.status == 200) {
+					location.reload();
+			 	}
+			}
+			xHttp.setRequestHeader("Content-type", "application/json");
+			xHttp.open("POST")
+			xHttp.send(data);
 
-					xHttp.open("POST","login.php");
-
-					xHttp.onreadystatechange  = function() {
-						if (this.readyState == 4 && this.status == 200) {
-							location.reload();
-					 	}
-					}
-
-					xHttp.send(data);
-			    });
-
-
-			},{scope: 'public_profile,email'});
-
-		}
+		});
 
 	</script>
 </body>
