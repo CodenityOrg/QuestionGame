4cea006f68f35de1a872217f54c7ac575e428a50
Franz <fcruzlau@gmail.com>
2017-04-25 15:43:39 -0500

    ranking

---
+++ points.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="css/ranking.css">
+</head>
+<body>
+    <header>
+       <h3>usuario</h3>
+        <div class="main">
+            <div class="uno"></div>
+            <div class="dos"></div>
+            <div class="tres"></div>
+        </div>
+    </header>
+    <div class="contenedor">
+       <div class="info">
+           <h1>RANKING</h1>
+           <section>
+               <h2>1</h2>
+               <input type="checkbox">
+               <h2>USUARIO 1</h2>
+               <h2>10p</h2>
+            </section>
+            <section>
+               <h2>2</h2>
+               <input type="checkbox">
+               <h2>USUARIO 2</h2>
+               <h2>5p</h2>
+            </section>
+       </div>
+        
+    </div>
+</body>
+</html>
