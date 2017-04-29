5267e5cf87323d1786f72d7f9a609a80977f2dbd
Franz <fcruzlau@gmail.com>
2017-04-29 18:03:05 -0500

    modalsad

---
+++ modal/sad.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="css/sad.css">
+</head>
+<body>
+    <div class="conteiner-sad">
+        <div class="info-sad">
+           <div class="sad-close">
+               <h1>X</h1>
+           </div>
+            <p>MALA SUERTE A LA PRÓXIMA CAMPEÓN :(</p>
+            <button class="sad-btn1">INTENTAR DE NUEVO</button>
+            <button class="sad-btn2">VOLVAMOS AL PRINCIPIO</button>
+        </div>
+    </div>
+</body>
+</html>
