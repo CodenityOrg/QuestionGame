26470a6c21a124bb18dccf97bfcdbd6267bb27d2
Xxedwin <edwin_limache@hotmail.com>
2017-05-06 18:09:02 -0500

    Merge branch 'master' of https://github.com/AngelRodRo/QuestionGameWebApp
    Conflicts:
    	.DS_Store
    	config/db.php
    	question.php

--- config/db.php
+++ config/db.php
@@ -4,11 +4,21 @@
 	define("HOST","127.0.0.1");
 	define("USERNAME", "root");
 <<<<<<< HEAD
+<<<<<<< HEAD
 	define("PASSWORD","casita");
 
 	try{
 		$query ="mysql:host=".HOST.";port=3306;dbname=".DB;
 		$con = new PDO("mysql:host=".HOST.";port=3306;dbname=".DB,USERNAME,PASSWORD);
+=======
+	define("PASSWORD","");
+
+	try{
+		$query ="mysql:host=".HOST.";port=3306;dbname=".DB;
+
+		$con = new PDO("mysql:host=".HOST.";port=3306;charset=UTF8;dbname=".DB,USERNAME,PASSWORD);
+
+>>>>>>> 9874e84e49897c429a0386d2afd57e84c4080566
 =======
 	define("PASSWORD","");
 
