551098fccf007257c4c42662b1aeea7eb0339f77
Xxedwin <edwin_limache@hotmail.com>
2017-05-06 18:34:12 -0500

    merge

--- config/db.php
+++ config/db.php
@@ -3,14 +3,6 @@
 	define("DB", "quiz");
 	define("HOST","127.0.0.1");
 	define("USERNAME", "root");
-<<<<<<< HEAD
-<<<<<<< HEAD
-	define("PASSWORD","casita");
-
-	try{
-		$query ="mysql:host=".HOST.";port=3306;dbname=".DB;
-		$con = new PDO("mysql:host=".HOST.";port=3306;dbname=".DB,USERNAME,PASSWORD);
-=======
 	define("PASSWORD","");
 
 	try{
@@ -18,16 +10,6 @@
 
 		$con = new PDO("mysql:host=".HOST.";port=3306;charset=UTF8;dbname=".DB,USERNAME,PASSWORD);
 
->>>>>>> 9874e84e49897c429a0386d2afd57e84c4080566
-=======
-	define("PASSWORD","");
-
-	try{
-		$query ="mysql:host=".HOST.";port=3306;dbname=".DB;
-
-		$con = new PDO("mysql:host=".HOST.";port=3306;charset=UTF8;dbname=".DB,USERNAME,PASSWORD);
-
->>>>>>> 9874e84e49897c429a0386d2afd57e84c4080566
 	} catch(PDOException $e) {
         echo '<p>'.$e->getMessage().'</p>';
 	    exit;
